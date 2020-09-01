3bc623c234a873c0e75c824af0b31eca72e86c8c
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2020-09-01 14:19:20 +0100

    Initial Commit with Model

---
+++ database/factories/DogFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Dog;
+use Faker\Generator as Faker;
+
+$factory->define(Dog::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
